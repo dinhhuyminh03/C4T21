fe5a5020a4fed6fb2f2052510bd1a927a00ee9b7
Đinh Huy Minh <dhuyminh@outlook.com.vn>
2019-06-14 16:39:37 +0700

    session 4

--- session3/practice_sum.py
+++ session3/practice_sum.py
@@ -5,5 +5,5 @@ r = 0
 for i in range (1, ending_number + 1):
     r = r + i
 print(r)
-# total_of_sum= 1 +ending_number
-# print("total: ", total_of_sum)
+total_of_sum= 1 +ending_number
+print("total: ", total_of_sum)
